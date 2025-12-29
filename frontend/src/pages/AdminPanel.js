030bb8394f0ad6ef3165b07f55580e5f99896b0e
Stylish <stylisticx02@gmail.com>
2025-12-29 18:49:50 +0000

    auto-commit for 4ebefff5-97f5-4db8-9f12-4e8ae41d98e0

--- frontend/src/pages/AdminPanel.js
+++ frontend/src/pages/AdminPanel.js
@@ -346,12 +346,21 @@ export const AdminPanel = () => {
                     className="w-full bg-muted/50 border border-white/10 px-4 py-3 text-white font-mono text-sm outline-none focus:border-primary"
                   >
                     <option value="Permanent">Permanent</option>
+                    <option value="5 minutes">5 minutes</option>
+                    <option value="10 minutes">10 minutes</option>
+                    <option value="15 minutes">15 minutes</option>
+                    <option value="30 minutes">30 minutes</option>
+                    <option value="1 hour">1 hour</option>
+                    <option value="2 hours">2 hours</option>
+                    <option value="6 hours">6 hours</option>
+                    <option value="12 hours">12 hours</option>
+                    <option value="1 day">1 day</option>
+                    <option value="3 days">3 days</option>
                     <option value="7 days">7 days</option>
+                    <option value="14 days">14 days</option>
                     <option value="30 days">30 days</option>
                     <option value="60 days">60 days</option>
                     <option value="90 days">90 days</option>
-                    <option value="1 hour">1 hour</option>
-                    <option value="1 day">1 day</option>
                   </select>
                 </div>
               </div>
